c35611cb96155874ea8dc2f663b9f1e5e881dcca
xyyx <xyyx@mail.ru>
2017-11-18 16:50:31 +0800

    santoni: Add Marlin's apn conf

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ TARGET_SCREEN_WIDTH := 720
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
+    device/xiaomi/santoni/configs/apns-full-conf.xml:system/etc/apns-conf.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
