07a9be32351633a178c1301c0a5f6366405816ae
infinity-plus <navdhareharsh2001@gmail.com>
2018-05-31 12:28:51 +0000

    cos-ify

--- cos.mk
+++ cos.mk
@@ -17,15 +17,15 @@
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := cos_santoni
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.4.0.NAMMIFA release-keys"
-
+    DEVICE_MAINTAINERS="@infinitEplus(TeamQuantum)"
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.4.0.NAMMIFA:user/release-keys"
