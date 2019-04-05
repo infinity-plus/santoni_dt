39a0a127ec398b2210ea4e0c2cb3521f94a7e569
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 17:50:43 +0530

    santoni: Un-blob libsensorndkbridge
    * Can be built from source

--- device.mk
+++ device.mk
@@ -409,7 +409,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # TextClassifier
 PRODUCT_PACKAGES += \
