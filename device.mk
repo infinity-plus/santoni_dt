9841b8d2b40d35fd06d4eeda09600ef47106fcd2
HarukeyUA <inazarrusnak@gmail.com>
2019-05-01 11:36:18 +0300

    Revert "santoni: Kill HIDL ANT+ support"
    Was working fine for us, oops
    
    This reverts commit ba8f70e4876273238ed20ec01e2937625039b211.

--- device.mk
+++ device.mk
@@ -136,8 +136,7 @@ PRODUCT_COPY_FILES += \
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 # Bluetooth
 PRODUCT_PACKAGES += \
